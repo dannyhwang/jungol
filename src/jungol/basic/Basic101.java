67532dc64125ac6684bf3b0191ebc34605e16a82
dannyhwang <wanabehacker@naver.com>
2017-01-02 16:25:35 +0900

    initial revision

---
+++ src/jungol/basic/Basic101.java
@@ -0,0 +1,9 @@
+package jungol.basic;
+
+public class Basic101 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is 홍길동");
+	}
+
+}
---
+++ src/jungol/basic/Basic102.java
@@ -0,0 +1,9 @@
+package jungol.basic;
+
+public class Basic102 {
+
+	public static void main(String[] args) {
+		System.out.println("나의 살던 고향은\n꽃피는 산골");
+	}
+
+}
