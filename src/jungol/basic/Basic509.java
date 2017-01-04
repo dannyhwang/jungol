7776522145146ea712663924581e9a354fad7819
dannyhwang <wanabehacker@naver.com>
2017-01-04 09:49:16 +0900

    Basic509 modify

--- src/jungol/basic/Basic509.java
+++ src/jungol/basic/Basic509.java
@@ -1,19 +1,9 @@
 package jungol.basic;
 
-import java.util.Scanner;
-
 public class Basic509 {
 
-	public static int n;
-	
 	public static void main(String[] args) {
-		getData();
+		int n = -100;
 		System.out.print(n);
-	}
-
-	public static void getData(){
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		sc.close();
 	}
 }
