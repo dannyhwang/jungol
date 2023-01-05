b49ec90f88113507dacd4d23e5a64ed796b22e29
jncc <jncc2020@naver.com>
2023-01-05 22:24:43 +0900

    initial revision

---
+++ src/jungol/basic/Basic103.java
@@ -0,0 +1,11 @@
+package jungol.basic;
+
+public class Basic103 {
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTT");
+		System.out.println("TTTTTTTTTT");
+		System.out.println("    TT");
+		System.out.println("    TT");
+		System.out.println("    TT");
+	}
+}
---
+++ src/jungol/basic/Basic104.java
@@ -0,0 +1,14 @@
+package jungol.basic;
+
+public class Basic104 {
+	public static void main(String[] args) {
+		int kor = 90;
+		int mat = 80;
+		int eng = 100;
+		
+		System.out.printf("kor %d\n", kor);
+		System.out.printf("mat %d\n", mat);
+		System.out.printf("eng %d\n", eng);
+		System.out.printf("sum %d\n", kor+mat+eng);
+	}
+}
